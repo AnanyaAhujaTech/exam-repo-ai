e40883133c0339ed1a55e10300c38edb5c4ce71c
Ananya Ahuja <tech.ananyaahuja@gmail.com>
2026-03-19 00:49:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from routes import search, question, repeated, paper
 
+app = FastAPI()
+
+app.include_router(search.router)
+app.include_router(question.router)
+app.include_router(repeated.router)
+app.include_router(paper.router)

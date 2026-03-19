6ee11bfa7b2693c953fb94f39d38b856843c27f6
Ananya Ahuja <tech.ananyaahuja@gmail.com>
2026-03-19 13:57:05 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@localhost/exam_db"
+# The connection string format is:
+# postgresql://username:password@host:port/database_name
 
+# Replace 'YOUR_PASSWORD' with the password you made during installation!
+DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/library_exam_db"
+
+# Create the engine
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
 
+# Create a session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency to use in your API routes (FastAPI)
 def get_db():
     db = SessionLocal()
     try:

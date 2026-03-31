31d563d31a87291868c2cd9d2c0655c4f4cf4eca
Ananya Ahuja <tech.ananyaahuja@gmail.com>
2026-03-31 19:58:47 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://neondb_owner:npg_9v1ueBkcryaF@ep-sweet-feather-an4wylqv-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = ""
 
 # Create the engine
 engine = create_engine(DATABASE_URL)

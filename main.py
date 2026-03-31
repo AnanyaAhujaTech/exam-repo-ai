be380d72a5a8dfa656dc89ec1df92b129c128971
Ananya Ahuja <tech.ananyaahuja@gmail.com>
2026-03-31 17:43:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from routes import search, question, repeated, paper, vector
+from routes import search, question, repeated, paper, vector, jobs
+from watcher import start_watcher
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    observer = start_watcher()
+    yield
+    observer.stop()
+    observer.join()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(search.router)
 app.include_router(question.router)
 app.include_router(repeated.router)
 app.include_router(paper.router)
 app.include_router(vector.router)
+app.include_router(jobs.router)
